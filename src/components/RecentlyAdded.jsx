c61528e1dacedb44d5484af70ab72ae8311137a5
BhavyaSareen <117308952+BhavyaSareen@users.noreply.github.com>
2024-09-22 12:00:30 +0530

    changes

--- src/components/RecentlyAdded.jsx
+++ src/components/RecentlyAdded.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // src/components/RecentlyAdded.jsx
 
 import React, { useState } from 'react';
@@ -44,40 +43,6 @@ const RecentlyAdded = ({ tasks, onEdit, onDelete }) => {
         />
       )}
     </div>
-=======
-import React from 'react';
-import { Table, Card, Container, Row, Col } from 'react-bootstrap';
-
-const RecentlyAdded = ({ tasks }) => {
-  return (
-    <Container className="mt-5">
-      <Row className="justify-content-md-center">
-        <Col md={8}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Recently Added Task List</Card.Title>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Due Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tasks?.slice(0,4).map(task => (
-                    <tr key={task.id}>
-                      <td>{task.title}</td>
-                      <td>{task.dueDate}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
->>>>>>> Stashed changes
   );
 };
 
